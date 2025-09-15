c73361d0c7ac6908077bd2a4eca7faefae6abbe6
matheusbck1010 <matheusbck1010@gmail.com>
2025-09-15 19:38:58 -0300

    Add files via upload

---
+++ projetoAcademia/src/main/java/com/projeto/projetoAcademia/repositorios/AlunoRepositorio.java
@@ -0,0 +1,11 @@
+package com.projeto.projetoAcademia.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.projetoAcademia.modelos.Aluno;
+
+public interface AlunoRepositorio extends JpaRepository<Aluno, Long>{
+
+	
+	
+}
